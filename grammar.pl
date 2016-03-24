880019f54079ac324135cb19041db40e61755d7f
daniel hao <Dan@DESKTOP-8005G3T.(none)>
2016-03-24 18:07:53 -0300

    Grammar file fixed

--- grammar.pl
+++ grammar.pl
@@ -1,30 +1,30 @@
 % apply Definite Clause Grammar
-program --> [functionList].
-functionList --> [function, functionListCollection].
-functionListCollection --> [functionList].
-functionListCollection --> [ ].
-function --> [typeID, '(', typeIDList, ')', '=', expression].
-typeID --> ['TYPE_INT','IDENTIFIER'].
-typeID --> ['TYPE_BOOL','IDENTIFIER'].
-typeIDList --> [typeID, typeIDListCollection].
-typeIDListCollection --> [',', typeIDList].
-typeIDListCollection --> [ ].
-expression --> ['if', comparison, 'then', value, 'else', value].
-expression --> ['let', 'IDENTIFIER', '=', value, 'in', expression].
-expression --> [value, extraExpression].
-extraExpression --> [arithmetic].
-extraExpression --> [ ].
-arithmetic --> ['+', value].
-arithmetic --> ['_', value].
-comparison --> [value, comparisonRight].
-comparisonRight --> ['==', value].
-comparisonRight --> ['!=', value].
-comparisonRight --> ['>', value].
-comparisonRight --> ['>=', value].
-value --> ['number'].
-value --> ['Identifier',　valueParameters].
-valueParameters --> ['(', parameters, ')'].
-valueParameters --> [ ].
-parameters --> [value, parametersList].
-parametersList --> [',', parameters].
-parametersList --> [ ].
+program --> functionList.
+functionList --> function, functionListCollection.
+functionListCollection --> functionList.
+functionListCollection --> [].
+function --> typeID, ['OPEN_P'], typeIDList, ['CLOSE_P'], ['ASSIGN'], expression.
+typeID --> ['TYPE_INT'],['IDENTIFIER'].
+typeID --> ['TYPE_BOOL'],['IDENTIFIER'].
+typeIDList --> typeID, typeIDListCollection.
+typeIDListCollection --> ['COMMA'], typeIDList.
+typeIDListCollection --> [].
+expression --> ['COND_IF'], comparison, ['COND_THEN'], value, ['COND_ELSE'], value.
+expression --> ['LET'], ['IDENTIFIER'], ['ASSIGN'], value, ['LET_IN'], expression.
+expression --> value, extraExpression.
+extraExpression --> arithmetic.
+extraExpression --> [].
+arithmetic --> ['ARITH_ADD'], value.
+arithmetic --> ['ARITH_SUB'], value.
+comparison --> value, comparisonRight.
+comparisonRight --> ['LOGIC_EQ'], value.
+comparisonRight --> ['LOGIC_NOT_EQ'], value.
+comparisonRight --> ['LOGIC_GT'], value.
+comparisonRight --> ['LOGIC_GTEQ'], value.
+value --> ['INTEGER'].
+value --> ['IDENTIFIER'], valueParameters.
+valueParameters --> ['OPEN_P'], parameters, ['CLOSE_P'].
+valueParameters --> [].
+parameters --> value, parametersList.
+parametersList --> ['COMMA'], parameters.
+parametersList --> [].
