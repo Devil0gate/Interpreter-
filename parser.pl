fac9cea3275a1e5ca6fa976574ab921e6d17722f
daniel hao <Dan@DESKTOP-8005G3T.(none)>
2016-03-27 01:08:27 -0300

    first modified parser

--- parser.pl
+++ parser.pl
@@ -1,3 +1,24 @@
 :-include('tokenizer.pl').
 :-include('lexer.pl').
+:-include('grammar.pl').
+
+% demo file for running parser
+run(FileName, StructuredList):-
+    tokenize_file(FileName, Tokens),
+    lexer(Tokens, LexedList),
+    parse_list(LexedList, StructuredList),
+    write(StructuredList).
+
+% output the structured list appropriately labeling tokens in production well written in lists
+% with given appropriate format
+parse_list(LexedList, Result):-
+    phrase(program(LexedList),StrcturedList,[]),
+    phrase(program(LexedList),StrcturedList,Result),
+    format('~s~n',[Result]).
+
+% write out grammar in formatted manner
+%    program(I)-->[I],{write_list(I),nl}.
+%    program(I)-->[I],{write_list(I)},program(I).    
+
+    
 
