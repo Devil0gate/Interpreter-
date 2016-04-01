b0525f8a0fb74c52a3e2d6b057c5ea671a612b03
daniel hao <Dan@DESKTOP-8005G3T.(none)>
2016-03-31 23:07:15 -0300

    fixed bugs for parser

--- parser.pl
+++ parser.pl
@@ -3,31 +3,42 @@
 :-include('grammar.pl').
 
 % demo file for running parser
-run(FileName, SList):-
+run(FileName):-
     tokenize_file(FileName, TokenList),
     lexer(TokenList, LexedList),
     phrase(program(SList),LexedList),
-    validate_rL(SList, TokenList).
-  
-% recap tokens    
-validate_rL([H|T],[H1|T1],[]):-
-    check_list([H|T],H,_),
-    replace_token([H|_],[H1|_]),
-    check_list([H|T],_,T),
-    replace_token([_|T],[_|T1]).
+    format(TokenList,SList,Result,_),
+    write(Result).
+%    validate_rL(SList, TokenList),
+%    write(SList).
+
+% recap tokens
+format(TokenList,[],[],TokenList).
+   format(TokenList,[H1|T1],[X|Y],TokenList_):-
+      is_list(H1),
+      format(TokenList,H1,X,Z),
+      format(Z,T1,Y,TokenList_).
+format([H|T],[_|T1],[H|Result],TokenList_0):-
+   format(T,T1,Result,TokenList_0).  
+    
+% validate_rL([H|T],[H1|T1]):-
+%    check_list([H|T],H,_),
+%    replace_token([H|_],[H1|_]),
+%    check_list([H|T],_,T),
+%    replace_token([_|T],[_|T1]).
 
 % check when reading a list or not
-check_list(X,[],[]):-
-check_list([H|T],H,T):-
-    check_list(H,_,_),
-    is_list(H),
-    check_list(T,_,_).
+% check_list(X,[],[]).
+% check_list([H|T],H,T):-
+%    check_list(H,_,_),
+%    is_list(H),
+%    check_list(T,_,_).
     
 % replace labels with right tokens
-replace_token([],[]):-
-replace_token([H|T],[H1|T1]):-
-    replace_token(H,H1),
-    replace_token(T,T1).
+% replace_token([],[]).
+% replace_token([H|T],[H1|T1]):-
+%    replace_token(H,H1),
+%    replace_token(T,T1).
         
 
     
